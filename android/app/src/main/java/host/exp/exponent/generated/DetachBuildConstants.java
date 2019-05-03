c43ef04db61fd131ddac79f9fd2694dad98ac06c
Ashley Heestand <heestand.ashley@gmail.com>
2019-05-03 15:13:17 -0500

    ejected

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp0cf8f678ac544386bef05f4a7512c30b://65-f4d.ashleyheestand.billsplitter.exp.direct:80";
+
+}

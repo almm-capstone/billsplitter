7f86b508aa5528036c29550edec33b502d08f14b
Ashley Heestand <heestand.ashley@gmail.com>
2019-05-07 14:52:17 -0500

    commiting

---
+++ secrets.js
@@ -0,0 +1,10 @@
+process.env.RESERVED_CLIENT_ID =
+  'com.googleusercontent.apps.997357993032-uaqtile974iu21kf5p5omes84l832ls2';
+
+process.env.GOOGLE_CLIENT_ID =
+  '833412404539-mbm0rv9d8i62711scjqjg7uusuie7bdq.apps.googleusercontent.com';
+
+process.env.ANDROID_CLIENT_ID =
+  '997357993032-sot47kjvcqge05ech3q763ehd00nboh4.apps.googleusercontent.com';
+
+process.env.FIREBASE_API_KEY = 'AIzaSyBwmylr4mJRaFpv12kF2DI-yQQhqT0dtcw';

18fbbed4100fddd0800cf758d30a542d216d36b2
LesleySmith <lesleysmith281@gmail.com>
2019-05-14 19:54:41 -0500

    fixed merge

--- src/store/receipts.js
+++ src/store/receipts.js
@@ -1,5 +1,4 @@
 export default {
-<<<<<<< HEAD
     receipts: [
       {
         placeholder: true,
@@ -17,22 +16,3 @@ export default {
       },
     ],
   };
-=======
-  receipts: [
-    {
-      placeholder: true,
-      id: 0,
-      title: "---- --- -- ------",
-      body:
-        "---- --- -- ------ ---- --- -- ------ ---- --- -- ------ ---- --- -- ------",
-      author: "---- ------ ",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/react-native-starter-app.appspot.com/o/image-1.jpg?alt=media&token=9f7c839b-2d40-4660-a2a0-bf6c2f64a2e5",
-      items: {
-        item: ["---- --- -- ------", "---- ------ --- --"]
-      },
-      users: []
-    }
-  ]
-};
->>>>>>> 72854b3202188b0ec34f9c642e47ff11c3cfd9ee
